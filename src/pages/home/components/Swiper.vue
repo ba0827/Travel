c2b64c3da681a454dac6932c91ea8e84638f087d
James Wenming <792031405@qq.com>
2018-05-16 22:42:29 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/c4/1cdd28811593b802.jpg_750x200_5fbb7c91.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/c4/1cdd28811593b802.jpg_750x200_5fbb7c91.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped="scoped">
+/*样式穿透*/
+.wrapper >>> .swiper-pagination-bullet-active {
+  background: #fff;
+}
+/*让元素的宽高比始终为31.25，为了解决网速过慢时页面抖动问题，设置height不行的*/
+.wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 31.25%;
+  background: #ccc;
+}
+
+.swiper-img {
+  width: 100%;
+}
+</style>
