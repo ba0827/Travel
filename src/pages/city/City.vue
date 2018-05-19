865c81ea754a5f101bfbea23243020f91f389257
James Wenming <792031405@qq.com>
2018-05-19 19:01:52 +0800

    city-router

---
+++ src/pages/city/City.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="city">
+  <city-header></city-header>
+</div>
+</template>
+
+<script>
+import CityHeader from './components/Header.vue'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped="scoped">
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -27,8 +29,8 @@ export default {
 @import '~styles/variables.styl'
 .header {
   display: flex;
-  height: .86rem;
-  line-height: .86rem;
+  height: $headerHeight;
+  line-height: $headerHeight;
   background: $bgColor;
   color: #fff;
 }
@@ -55,6 +57,7 @@ export default {
   width: 1.24rem;
   float: right;
   text-align: center;
+  color: #fff;
 }
 .arrow-icon {
   margin-left: -0.04rem;
